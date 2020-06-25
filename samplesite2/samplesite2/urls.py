1760cd1c2f83052ce4294a461c3e3402221695af
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-06-25 19:48:44 +0700

    add forms for items

--- samplesite2/samplesite2/urls.py
+++ samplesite2/samplesite2/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from bboard.views import index
+from bboard.views import by_rubric
+from bboard.views import BbCreateView
 
 urlpatterns = [
-    path('bboard/', index),
+    path('bboard/<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('bboard/', index, name='index'),
+    path('bboard/add/', BbCreateView.as_view(), name='add'),
     path('admin/', admin.site.urls),
 ]
