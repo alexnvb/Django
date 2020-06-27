67c9cff239b678317471305426c106029969ae37
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-06-27 18:16:39 +0700

    db work

--- rss/rss/apps/rss_news/models.py
+++ rss/rss/apps/rss_news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    title = models.CharField(max_length=60)
+    text = models.TextField()
+
+    def __str__(self):
+        return str(self.id)+" "+str(self.title)
