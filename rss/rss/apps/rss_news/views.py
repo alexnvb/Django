92ba069365d8e8f887e46d825b91f2a02639995e
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-06-27 16:08:01 +0700

    add bootstrap3 and main page

--- rss/rss/apps/rss_news/views.py
+++ rss/rss/apps/rss_news/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import random
+
 
 # Create your views here.
+
+def index(request):
+    number = random.randrange(0, 100)
+
+    context = {
+    }
+
+    return render(request, "index.html", context)
