c773f700227e18b690a1d9fe18cd60d6a71a14dc
alexnvb <alexgridin@mail.ru>
2020-06-28 19:20:24 +0700

    create models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOISE = (
+        ('draft', 'draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOISE, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+    def __str__(self):
+        return self.title
