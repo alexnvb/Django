69a8d683c8f04cf1090bdd5f6de5d80f06748dfc
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-06-30 10:03:16 +0700

    Form and model and views

--- test_ptoject/landing/models.py
+++ test_ptoject/landing/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
-class Subscribers(models.Model):
+class Subscriber(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "Пользователь %s %s" % (self.name, self.email,)
+
+    class Meta:
+        verbose_name = 'MySubscriber'
+        verbose_name_plural = 'A lot of Subscribers'
--- test_ptoject/landing/views.py
+++ test_ptoject/landing/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .forms import SubscribersForm
+from .forms import SubscriberForm
+
 
 def landing(request):
-    form = SubscribersForm(request.POST or None)
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        data  = form.cleaned_data
+
+        new_form = form.save()
+
     return render(request, 'landing/landing.html', locals())
