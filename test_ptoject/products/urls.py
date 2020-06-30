a287ec8a02b1da2298274c1174eefa25caa71454
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-06-30 11:24:58 +0700

    add

---
+++ test_ptoject/products/urls.py
@@ -0,0 +1,15 @@
+# from django.conf.urls import path, url, include
+# from django.contrib import admin
+# from landing import views
+#
+# urlpatterns = [
+#     path('landing/', views.landing, name='landing')
+# ]
+
+from django.contrib import admin
+from django.urls import path, include
+from landing import views
+
+urlpatterns = [
+#    path('landing/', views.landing, name='landing'),
+]
